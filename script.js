d41277dcded527d88119dc0fe0ea736df2614dfc
Emiel <emiel.delaey@student.howest.be>
2024-07-06 20:50:29 +0200

    update styling

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('paste', function(event) {
+  var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+
+  for (index in items) {
+      var item = items[index];
+      if (item.kind === 'file') {
+          var blob = item.getAsFile();
+          if (blob.type.indexOf('image') !== -1) {
+              var reader = new FileReader();
+              reader.onload = function(event) {
+                  var imageData = event.target.result;
+                  setImage(imageData)
+              };
+              reader.readAsDataURL(blob);
+          }
+      }
+  }
+});
+
+
+function setImage(imageData) {
+  var imgElement = document.createElement('img');
+  imgElement.src = imageData;
+  $imgContainer = document.querySelector("#img")
+  $imgContainer.innerHTML = '';
+  $imgContainer.insertAdjacentElement('beforeEnd', imgElement);
+}
